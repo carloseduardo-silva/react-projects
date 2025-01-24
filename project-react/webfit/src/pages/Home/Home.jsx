9137ede26540e3da354bf027b2afc2ecef64bae6
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2025-01-24 00:22:24 -0300

    First day working on webfit project 23/01

--- project-react/webfit/src/pages/Home/Home.jsx
+++ project-react/webfit/src/pages/Home/Home.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import styles from "./Home.module.css"
 export const Home = () => {
   return (
-    <div className={styles.home_container}>
+    <>
+    
+    <div className={styles.home_container}></div>
+    
+    <div className={styles.description}>
 
-      <h1>Banner</h1>
+        <h4 className='cursive'>Lorem ipsum dolor</h4>
+      <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia nobis, ut repellendus possimus, expedita numquam quod ipsam, atque libero provident dicta! Ad recusandae voluptatem voluptate repudiandae ducimus perspiciatis aliquid amet?</p>
     </div>
+    
+    <div className={styles.banner_secundario}></div>
+
+    </>
+
   )
 }
